ee188026bd412b621f69389c45346bc63633c2a6
Michał <michal@kaftanowicz.com>
2017-10-22 23:33:13 +0200

    Updated .jl file

--- solow_growth_model.jl
+++ solow_growth_model.jl
@@ -4,41 +4,25 @@ using Plots
 
 # Functions declaration
 
-function CobbDouglas(K, AL, alpha, beta)
-    # Cobb-Douglas production function
-    K^alpha * AL^beta
-end
+# Cobb-Douglas production function
+# Default beta = 1-alpha gives constant return to scale (CRS)
+CobbDouglas(K, AL, alpha, beta = 1-alpha) = K^alpha * AL^beta
 
-function CobbDouglasCRS(K, AL, alpha)
-    # Cobb-Douglas production function with constant returns to scale
-    CobbDouglas(K, AL, alpha, 1-alpha)
-end
+# Production function to be used in model
+Production(K, A, L) = CobbDouglas(K, A*L, 0.7)
+    
+# Capital in (t+1) as function of Capital in (t), Investment in (t)
+# and depreciation rate delta
+CapitalChange(K,Inv, delta) = (1-delta)*K + Inv
 
-function Production(K, A, L)
-    # Production function
-    CobbDouglasCRS(K, A*L, 0.7)
-end
+# Investment (equal to savings) as function of Production in (t) and savings rate s
+Investment(Y, s) = s * Y
 
-function CapitalChange(K, I, delta)
-    # Capital in (t+1) as function of Capital in (t), Investment in (t)
-    # and depreciation rate delta
-    (1-delta)*K + I
-end
+# Labour in (t+1) as function of Labour in (t) and growth rate of population n
+LabourChange(L, n) = (1+n)*L
 
-function Investment(Y, s)
-    # Investment (equal to savings) as function of Production in (t) and savings rate s
-    s * Y
-end
-
-function LabourChange(L, n)
-    # Labour in (t+1) as function of Labour in (t) and growth rate of population n
-    (1+n)*L
-end
-
-function TechnologyChange(A, g)
-    # Technology in (t+1) as function of Technology in (t) and growth rate of technology g
-    (1+g)*A
-end
+# Technology in (t+1) as function of Technology in (t) and growth rate of technology g
+TechnologyChange(A, g) = (1+g)*A
 
 function ExtractParameters(parameters_dict)
     # Takes a Dictionary and creates variables with names corresponding to its key names
@@ -58,50 +42,56 @@ function SolveFirmsProblem(ProductionFunction, K, A, L)
     W, Rk
 end
 
+mutable struct model_parameters
+s::Float64
+n::Float64
+g::Float64
+delta::Float64
+end
+
+parameters = model_parameters(0.04, 0.02, 0.01, 0.01)
+
 function Solow(K0, A0, L0, parameters, T)
-    ExtractParameters(parameters)
-    
     # In t = 0:
-    Y0 = Production(K0, A0*L0)
-    I0 = Investment(Y0, s)
-    C0 = Y0 - I0 # Goods market clearing condition
+    Y0 = Production(K0, A0, L0)
+    Inv0 = Investment(Y0, parameters.s)
+    C0 = Y0 - Inv0 # Goods market clearing condition
     W0, Rk0 = SolveFirmsProblem(Production, K0, A0, L0)
     
     # Initializing sequences of allocations and prices for times t=1..T
-    for x = [:C, :Y, :K, :I, :Rk, :W, :A, :L]
+    for x = [:C, :Y, :K, :Inv, :Rk, :W, :A, :L]
         @eval $x = zeros(T)
     end
     
     t = 1
-    K[t] = CapitalChange(K0, I0, delta)
-    A[t] = TechnologyChange(A0, g)
-    L[t] = LabourChange(L0, n)
+    K[t] = CapitalChange(K0, Inv0, parameters.delta)
+    A[t] = TechnologyChange(A0, parameters.g)
+    L[t] = LabourChange(L0, parameters.n)
     
-    Y[t] = Production(K[t], A[t]*L[t])
-    I[t] = Investment(Y[t], s)
-    C[t] = Y[t] - I[t] # Goods market clearing condition
+    Y[t] = Production(K[t], A[t], L[t])
+    Inv[t] = Investment(Y[t], parameters.s)
+    C[t] = Y[t] - Inv[t] # Goods market clearing condition
     W[t], Rk[t] = SolveFirmsProblem(Production, K[t], A[t], L[t])
     
     for t = 2:T
-        K[t] = CapitalChange(K[t-1], I[t-1], delta)
-        A[t] = TechnologyChange(A[t-1], g)
-        L[t] = LabourChange(L[t-1], n)
+        K[t] = CapitalChange(K[t-1], Inv[t-1], parameters.delta)
+        A[t] = TechnologyChange(A[t-1], parameters.g)
+        L[t] = LabourChange(L[t-1], parameters.n)
         
-        Y[t] = Production(K[t], A[t]*L[t])
-        I[t] = Investment(Y[t], s)
-        C[t] = Y[t] - I[t] # Goods market clearing condition
+        Y[t] = Production(K[t], A[t], L[t])
+        Inv[t] = Investment(Y[t], parameters.s)
+        C[t] = Y[t] - Inv[t] # Goods market clearing condition
         W[t], Rk[t] = SolveFirmsProblem(Production, K[t], A[t], L[t])
     end
     
-    C, Y, K, I, Rk, W, A, L
+    C, Y, K, Inv, Rk, W, A, L
 end
 
 K0 = 100
 A0 = 1.1
 L0 = 300
-parameters = Dict("s" => 0.04, "n" => 0.02, "g" => 0.01, "delta" => 0.01)
 T = 100
 
-C, Y, K, I, Rk, W, A, L = Solow(K0, A0, L0, parameters, T)
+C, Y, K, Inv, Rk, W, A, L = Solow(K0, A0, L0, parameters, T)
 
 plot(C, color="blue")
